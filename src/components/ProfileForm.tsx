4a4b60acc802460fbaf535b4fdcf15a9176dc935
erikfazio <fazioerik@gmail.com>
2023-11-24 15:58:56 +0100

    Fix

---
+++ src/components/ProfileForm.tsx
@@ -0,0 +1,56 @@
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+import useUpdateUserByUserIdMutation from "@/hooks/users/useUpdateUserByUserIdMutation";
+import { useAuth } from "@/context/AuthProvider";
+
+type FormData = {
+  first_name: string;
+  last_name: string;
+};
+
+const ProfileForm = ({ defaultValues }: any) => {
+  console.log(defaultValues);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      first_name: defaultValues.first_name,
+      last_name: defaultValues.last_name,
+    },
+  });
+  const { user } = useAuth();
+  const updateProfile = useUpdateUserByUserIdMutation();
+
+  const handleUpdateProfile = (data: FormData) => {
+    console.log(data);
+    updateProfile.mutate({
+      id: user.id,
+      ...data,
+    });
+  };
+
+  return (
+    <form
+      className="container mx-auto flex flex-col gap-y-8"
+      onSubmit={handleSubmit(handleUpdateProfile)}
+    >
+      <Input
+        type="text"
+        placeholder="First name"
+        {...register("first_name", { required: true })}
+      />
+      {errors.first_name && <span>This field is required</span>}
+      <Input
+        type="text"
+        placeholder="Last name"
+        {...register("last_name", { required: true })}
+      />
+      {errors.last_name && <span>This field is required</span>}
+      <Input type="submit" />
+    </form>
+  );
+};
+
+export default ProfileForm;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,58 +1,20 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { useAuth } from "../context/AuthProvider";
-import { Input } from "@/components/ui/input";
-import useUsersQuery from "@/hooks/users/useUsersQuery";
 import useUserByIdQuery from "@/hooks/users/useUserByIdQuery";
-import { useForm } from "react-hook-form";
-import useUpdateUserByUserIdMutation from "@/hooks/users/useUpdateUserByUserIdMutation";
+
+import ProfileForm from "@/components/ProfileForm";
 
 function Profile() {
   const { data: profile, isLoading: isProfileLoading } = useUserByIdQuery();
-  const { user } = useAuth();
-  const updateProfile = useUpdateUserByUserIdMutation();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
 
-  const handleUpdateProfile = (data: any) => {
-    console.log(data);
-    updateProfile.mutate({
-      id: user.id,
-      ...data,
-    });
-  };
+  console.log(profile);
 
   return (
     <main className="mt-16 container mx-auto flex flex-col gap-y-8">
-      {!isProfileLoading && (
+      {!isProfileLoading && profile && (
         <>
           <div className="flex justify-between">
             <h1 className="font-bold text-4xl">Profile</h1>
-            <Button variant="outline" onClick={handleUpdateProfile}>
-              Update profile
-            </Button>
           </div>
-          <form
-            className="container mx-auto flex flex-col gap-y-8"
-            onSubmit={handleSubmit(handleUpdateProfile)}
-          >
-            <Input
-              type="text"
-              placeholder="First name"
-              {...register("first_name", { required: true })}
-            />
-            {errors.first_name && <span>This field is required</span>}
-            <Input
-              type="text"
-              placeholder="Last name"
-              {...register("last_name", { required: true })}
-            />
-            {errors.last_name && <span>This field is required</span>}
-            <Input type="submit" />
-          </form>
+          {!isProfileLoading && <ProfileForm defaultValues={profile} />}
         </>
       )}
     </main>
