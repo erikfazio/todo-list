5d592ebd6907ec6cc2a3271a903a0f72758c226e
erikfazio <fazioerik@gmail.com>
2023-11-24 10:42:25 +0100

    Implement update of a user profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,31 +4,48 @@ import { useAuth } from "../context/AuthProvider";
 import { Input } from "@/components/ui/input";
 import useUsersQuery from "@/hooks/users/useUsersQuery";
 import useUserByIdQuery from "@/hooks/users/useUserByIdQuery";
+import useUpdateUserByUserIdMutation from "@/hooks/users/useUpdateUserByUserIdMutation";
 
 function Profile() {
   const { data: profile, isLoading: isProfileLoading } = useUserByIdQuery();
   const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState(profile?.["last_name"]);
+  const [lastName, setLastName] = useState();
   const { user, isAdmin, signOut } = useAuth();
   const { data: users, isLoading: isUsersLoading } = useUsersQuery();
+  const updateProfile = useUpdateUserByUserIdMutation();
 
-  console.log(profile);
+  const handleUpdateProfile = () => {
+    updateProfile.mutate({
+      id: user.id,
+      first_name: firstName,
+      last_name: lastName,
+    });
+  };
 
   return (
     <main className="mt-16 container mx-auto flex flex-col gap-y-8">
       <div className="flex justify-between">
         <h1 className="font-bold text-4xl">Profile</h1>
-        <Button variant="outline">Update profile</Button>
+        <Button variant="outline" onClick={handleUpdateProfile}>
+          Update profile
+        </Button>
       </div>
       <div className="container mx-auto flex flex-col gap-y-8">
-        <Input type="text" placeholder="First name" value={firstName} />
-        <Input type="text" placeholder="Last name" value={lastName} />
+        <Input
+          type="text"
+          placeholder="First name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
     </main>
   );
 }
 
 export default Profile;
-function useProfileQuery(): { data: any; isLoading: any } {
-  throw new Error("Function not implemented.");
-}
--- src/queries/users.ts
+++ src/queries/users.ts
@@ -8,6 +8,7 @@ export function getUserById(client: SupabaseClient, id: string) {
   return client.from("users").select("*").eq("id", id);
 }
 
-export function updateUserById(client: SupabaseClient, id: string, data: any) {
+export function updateUserById(client: SupabaseClient, data: any) {
+  const { id } = data;
   return client.from("users").update(data).eq("id", id);
 }
